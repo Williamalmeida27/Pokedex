211827a4f7dac77898c5dda18397a1077466e9fe
Williamalmeida27 <jordan.wa18@gmail.com>
2023-03-25 20:28:55 -0300

    Incialize

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+convertToListHtml = (pokemon) => {
+    return `
+    <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+
+                    </ol>
+
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                        alt="${pokemon.name}">
+                </div>
+
+            </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+
+apiPoke.getPokemon().then((pokemons) => {
+    const ListPokemon = []
+
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        ListPokemon.push()
+        pokemonList.innerHTML += convertToListHtml(pokemon)
+
+    }
+})
---
+++ assets/js/poke-Api.js
@@ -0,0 +1,12 @@
+
+const apiPoke = {}
+
+apiPoke.getPokemon = (offset = 0, limit = 10) => {
+    const URL = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+    return fetch(URL)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .catch((error) => console.error(error))
+
+}
